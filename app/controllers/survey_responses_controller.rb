900f61b5045e860012764fa44472ee310a7103be
Coraline <coraline@idolhands.com>
2024-04-16 21:50:50 -0500

    Fix an issue with tag fields crossing wires

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -37,6 +37,7 @@ class SurveyResponsesController < ApplicationController
 	def question
 		@question_label = params.permit(:q)[:q]
 		@question_readable = SurveyResponse::QUESTION_MAPPING[params.permit(:q)[:q].to_sym] || @question_label.to_sym
+		@question_field = "#{@question_label.gsub(/given/, 'id')}_tags".to_sym
 		@responses = SurveyResponse.all.order(:created_at).reject{|sr| sr.read_attribute(@question_label).nil? }
 	end
 		
