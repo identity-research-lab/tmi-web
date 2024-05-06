48e927b6f0014d3f55f263d791438d0250053d04
CoralineAda <coraline@idolhands.com>
2024-05-05 23:20:17 -0500

    Extract question-related logic to a question object

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,10 +11,8 @@ class QuestionsController < ApplicationController
 	end
 
 	def show
-		@question_label = params[:id]
-		@question_readable = SurveyResponse::QUESTION_MAPPING[params[:id].to_sym] || @question_label.to_sym
-		@question_field = "#{@question_label.gsub(/given/, 'id')}_tags".to_sym
-		@responses = SurveyResponse.all.order(:created_at).reject{|sr| sr.read_attribute(@question_label.to_sym).nil? }
+		@question = Question.from(params[:id])
+		@responses = SurveyResponse.all.order(:created_at).reject{|sr| sr.read_attribute(@question.key).nil? }
 	end
 			
 end
