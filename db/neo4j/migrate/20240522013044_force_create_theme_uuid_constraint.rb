8d3dbf96cbdb5ef1b4685edaf83e80257b768ea2
CoralineAda <coraline@idolhands.com>
2024-06-08 15:16:24 -0500

    HOTPATCH - bad neo4j migration

--- db/neo4j/migrate/20240522013044_force_create_theme_uuid_constraint.rb
+++ db/neo4j/migrate/20240522013044_force_create_theme_uuid_constraint.rb
@@ -1,9 +1,9 @@
 class ForceCreateThemeUuidConstraint < ActiveGraph::Migrations::Base
   def up
-    add_constraint :Theme, :uuid, force: true
+#    add_constraint :Theme, :uuid, force: true
   end
 
   def down
-    drop_constraint :Theme, :uuid
+#    drop_constraint :Theme, :uuid
   end
 end
