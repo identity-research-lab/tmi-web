00f1b8c42de1000e2ff94ebed5e91e1151b3c56b
CoralineAda <coraline@idolhands.com>
2024-06-15 18:08:33 -0500

    Refactor keyword#from

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -25,33 +25,25 @@ class Keyword
 	def self.from(survey_response_id)
 		survey_response = SurveyResponse.find(survey_response_id)
 		persona = Persona.find_by(survey_response_id: survey_response_id)
-		
+		corpus = Question.freeform_questions.map{ |q| survey_response.send(q) }.compact.join(" .")
 		client = OpenAI::Client.new
 
-		questions = Question.freeform_questions
-		
-		questions.each do |question|
-			
-			next unless corpus = survey_response.send(question.to_sym)
-										
-			response = client.chat(
-				parameters: {
-					model: "gpt-4o",
-					response_format: { type: "json_object" },
-					messages: [{ role: "user", content: "#{PROMPT} #{corpus}" }],
-					temperature: 0.7,
-				}
-			)	
+		response = client.chat(
+			parameters: {
+				model: "gpt-4o",
+				response_format: { type: "json_object" },
+				messages: [{ role: "user", content: "#{PROMPT} #{corpus}" }],
+				temperature: 0.7,
+			}
+		)	
 	
-			data = JSON.parse(response.dig("choices", 0, "message", "content"))['words']
-	
-			data.compact.uniq.each do |word|
-				if keyword = Keyword.find_or_create_by(name: word.downcase)
-					DescribesWith.create(from_node: persona, to_node: keyword )
-				end
+		data = JSON.parse(response.dig("choices", 0, "message", "content"))
+		data['words'].compact.uniq.each do |word|
+			if keyword = Keyword.find_or_create_by(name: word.downcase)
+				DescribesWith.create(from_node: persona, to_node: keyword )
 			end
-
 		end
+
 	end
 
 end 
