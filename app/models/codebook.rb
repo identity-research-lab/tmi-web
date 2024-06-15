884ca906497279c908ce19379b1979c77bcdecd0
CoralineAda <coraline@idolhands.com>
2024-06-15 14:51:35 -0500

    Explainers to all graph queries

--- app/models/codebook.rb
+++ app/models/codebook.rb
@@ -1,11 +1,10 @@
 class Codebook
   
   def self.category_query(context)
-    "MATCH (cat:Category)-[:CATEGORIZES_AS]-(c:Code) WHERE cat.context=\"#{context}\" RETURN cat,c"  
-  end
-
-  def self.category_query_explainer(context)
-    "// Show me a graph of \"#{context}\"-related categories and their associated codes."
+    {
+      explainer: "// Show me a graph of \"#{context}\"-related categories and their associated codes.",
+      query: "MATCH (cat:Category)-[:CATEGORIZES_AS]-(c:Code) WHERE cat.context=\"#{context}\" RETURN cat,c" 
+    }
   end
 
 end
