c9184ab22fff316c0abaa3ae21bfb6cb2a70f7c8
CoralineAda <coraline@idolhands.com>
2024-06-18 12:49:08 -0500

    Increment of love

--- lib/clients/open_ai.rb
+++ lib/clients/open_ai.rb
@@ -1,4 +1,10 @@
+# A wrapper class for the OpenAI API.
 class Clients::OpenAi
+
+  # Sends a prompt to the configured model and returns the relevant portion of the response.
+  #
+  # @param prompt [String] the text of the prompt to send to the model.
+  # @return [String] the JSON response returned by the API.
   
   def self.request(prompt)
     client = OpenAI::Client.new
