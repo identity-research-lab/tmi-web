738396771bac57c9e04f38e65ff0a69b51631276
Coraline <coraline@idolhands.com>
2024-06-25 16:50:28 -0400

    Correct the edge name for category-code sample query

--- app/models/codebook.rb
+++ app/models/codebook.rb
@@ -3,7 +3,7 @@ class Codebook
   def self.category_query(context)
     {
       explainer: "// Show me a graph of \"#{context}\"-related categories and their associated codes.",
-      query: "MATCH (cat:Category)-[:CATEGORIZES_AS]-(c:Code) WHERE cat.context=\"#{context}\" RETURN cat,c" 
+      query: "MATCH (cat:Category)-[:CATEGORIZED_AS]-(c:Code) WHERE cat.context=\"#{context}\" RETURN cat,c" 
     }
   end
 
