0995bb4311460cb56da3fdcf4025b6e6ea2d6d23
Coraline <coraline@idolhands.com>
2024-08-27 14:28:23 -0500

    Move basic auth to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+	
+	before_action :authenticate
+
+	def authenticate
+		Rails.logger.info("!!! => #{ENV.fetch('GENERAL_ADMISSION_PROTECTED').inspect}")
+		return unless ENV.fetch('GENERAL_ADMISSION_PROTECTED') == "true"
+		authenticate_or_request_with_http_digest("Application") do |name|
+			USERS[name]
+		end
+	end
+
 end
