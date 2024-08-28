f14c39f81474f6023cfd5fd2ffe14b7950b407d5
CoralineAda <coraline@idolhands.com>
2024-08-28 13:53:51 -0500

    Set a default value on the env variable fetch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-	
+
 	before_action :authenticate
 
 	# If http auth is enabled, challenge the browser for a username and password as defined in the environment variables.
 	def authenticate
-		return unless ENV.fetch('GENERAL_ADMISSION_PROTECTED') == "true"
+		return unless ENV.fetch('GENERAL_ADMISSION_PROTECTED',nil) == "true"
 		authenticate_or_request_with_http_digest("Application") do |name|
 			USERS[name]
 		end
