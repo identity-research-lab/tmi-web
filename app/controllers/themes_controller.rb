067ded4cb4fe1a0b5a317d9553beb7c9108fe990
CoralineAda <coraline@idolhands.com>
2024-10-15 17:42:00 -0500

    Ability to add a new theme

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -12,6 +12,12 @@ class ThemesController < ApplicationController
     @codes = Code.where(context: @context)
   end
 
+  def create
+    @theme = Theme.new(theme_params)
+    success = @theme.save
+    redirect_to action: :show, id: @theme.context
+  end
+
   def update
     @theme = Theme.find(params[:id])
 
@@ -24,6 +30,12 @@ class ThemesController < ApplicationController
         render turbo_stream: turbo_stream.replace("frame-theme-#{@theme.id}", partial: "/themes/form", locals: { theme: @theme  })
       end
     end
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:name, :context)
   end
 
 end
