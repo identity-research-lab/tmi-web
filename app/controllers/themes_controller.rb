f0975a1d51dc7b0d3405548f33af8667d63e334c
Coraline <coraline@idolhands.com>
2024-10-21 15:21:09 -0500

    Interface for updating theme metadata and categories

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -18,15 +18,23 @@ class ThemesController < ApplicationController
 
   def update
     @theme = Theme.find(params[:id])
+    new_category_ids = theme_params[:categories].split(/[\,\s]/)    
+    update_kind = @theme.categories.length == new_category_ids.length ? "metadata" : "category"
 
-    categories = Category.where(id: theme_params[:categories].split(','))
-    @theme.categories = categories
+    if theme_params[:categories]
+      categories = Category.where(id: new_category_ids)
+      @theme.categories = categories
+    end
+    
     @theme.name = theme_params[:name]
-    @theme.save
+    @theme.description = theme_params[:description]
+    @theme.notes = theme_params[:notes]
+    update_kind ||= "metadata"
+    success = @theme.save
 
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("frame-theme", partial: "/themes/form", locals: { theme: @theme  })
+        render turbo_stream: turbo_stream.replace("frame-theme", partial: "/themes/form", locals: { theme: @theme, success: success, update_kind: update_kind })
       end
     end
   end
