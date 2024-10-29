e6781400e2c95bdc6c259524ee5e3a5b54115b4d
CoralineAda <coraline@idolhands.com>
2024-10-29 01:01:49 -0500

    Don't enqueue export to graph on response db migration, take 2

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,7 @@
 class Response < ApplicationRecord
 
   # TODO I need this to run under normal circumstances but NOT during the db migration that creates the records.
-  after_save_commit :enqueue_export_to_graph
+  after_update :enqueue_export_to_graph
 
   belongs_to :survey_response
   belongs_to :question
@@ -14,7 +14,7 @@ class Response < ApplicationRecord
 
     # Invokes a service to update the graph databases from the associated SurveyResponse object.
     def enqueue_export_to_graph
-      ExportToGraphJob.perform_async(self.survey_response_id)
+      SurveyResponse.find(self.survey_response_id).save
     end
 
 end
