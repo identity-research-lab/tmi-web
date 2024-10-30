587ea220d5480decc9358d8c22eb8be969198335
CoralineAda <coraline@idolhands.com>
2024-10-30 14:36:34 -0500

    Cache value of categories lookup

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -3,21 +3,21 @@
 
 class Persona
   include ActiveGraph::Node
-  
+
   property :name
   property :survey_response_id
   property :permalink
-  
+
   validates :name, presence: true
   validates :survey_response_id, presence: true
 
   has_many :out, :codes, rel_class: :Experiences, dependent: :delete_orphans
   has_many :out, :identities, rel_class: :IdentifiesWith, dependent: :delete_orphans
   has_many :out, :keywords, rel_class: :ReflectsOn, dependent: :delete_orphans
-  
+
   # Traverses the codes-[]-categories relationship to return a list of unique Categories.
   def categories
-    self.codes.map(&:categories).flatten.uniq
+    @categories ||= self.codes.map(&:categories).flatten.uniq
   end
-  
+
 end
