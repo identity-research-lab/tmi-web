499b76e57f91f229ea046d084df8a5a8da5febfb
Coraline <coraline@idolhands.com>
2024-12-09 19:41:17 -0600

    Fix query explainer on codebook page

--- app/models/codebook.rb
+++ app/models/codebook.rb
@@ -4,8 +4,8 @@ class Codebook
   # Used to label and display a Category query.
   def self.category_query(context)
     {
-      explainer: "// Show me a graph of \"#{context}\"-related categories and their associated codes.",
-      query: "MATCH (cat:Category)-[:CATEGORIZED_AS]-(c:Code) WHERE cat.context=\"#{context}\" RETURN cat,c"
+      explainer: "// Show me a graph of \"#{context.display_name.downcase}\" categories and their associated codes.",
+      query: "MATCH (cat:Category)-[:CATEGORIZED_AS]-(c:Code) WHERE cat.context=\"#{context.name}\" RETURN cat,c"
     }
   end
 
