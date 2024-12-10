eee95c913ce277fdd80758e35ee0b9c50d6a3f96
Coraline <coraline@idolhands.com>
2024-12-09 21:01:33 -0600

    Make Responses a top-level nav concern

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,9 @@
 class QuestionsController < ApplicationController
 
+  def index
+    @questions = Question.all
+  end
+  
   def show
     @question = Question.find(params[:id])
     @responses = @question.responses.order(:case_id)
