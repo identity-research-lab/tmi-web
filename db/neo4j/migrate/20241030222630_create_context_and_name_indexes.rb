3fc0652c55e635e9f13a8d51e8dd136f48e89ba6
CoralineAda <coraline@idolhands.com>
2025-03-31 11:39:58 -0500

    Neo4j db changes

--- db/neo4j/migrate/20241030222630_create_context_and_name_indexes.rb
+++ db/neo4j/migrate/20241030222630_create_context_and_name_indexes.rb
@@ -1,23 +1,23 @@
 class CreateContextAndNameIndexes < ActiveGraph::Migrations::Base
 
   def up
-    add_index :Category, :context
-    add_index :Category, :name
-    add_index :Code, :context
-    add_index :Code, :name
-    add_index :Identity, :context
-    add_index :Identity, :name
-    add_index :Theme, :name
+    # add_index :Category, :context
+    # add_index :Category, :name
+    # add_index :Code, :context
+    # add_index :Code, :name
+    # add_index :Identity, :context
+    # add_index :Identity, :name
+    # add_index :Theme, :name
   end
 
   def down
-    remove_index :Category, :context
-    remove_index :Category, :name
-    remove_index :Code, :context
-    remove_index :Code, :name
-    remove_index :Identity, :context
-    remove_index :Identity, :name
-    remove_index :Theme, :name
+    drop_index :Category, :context
+    drop_index :Category, :name
+    drop_index :Code, :context
+    drop_index :Code, :name
+    drop_index :Identity, :context
+    drop_index :Identity, :name
+    drop_index :Theme, :name
   end
 
 end
